4a58787bbe1d1c7c0ab6c75f718bd727f7406af9
Ahmed saied <ahmed.muhammed.s@hotmail.com>
2021-10-01 01:46:32 +0300

    test cases

--- Boling.Test/ScoreTest.cs
+++ Boling.Test/ScoreTest.cs
@@ -25,5 +25,44 @@ namespace Bowling.Test
             game.Roll(4);
             Assert.Equal(6, game.GetScore());
         }
+
+        [Fact]//(Skip = "SpareAddsNextBall")
+        public void SpareAddsNextBall()
+        {
+            game.Roll(6);
+            game.Roll(4);
+            game.Roll(3);
+            game.Roll(0);
+            Assert.Equal(16, game.GetScore());
+        }
+
+        [Fact]
+        public void ATenInTwoFramesIsNotaSpare()
+        {
+            game.Roll(3);
+            game.Roll(6);
+            game.Roll(4);
+            game.Roll(2);
+            Assert.Equal(15, game.GetScore());
+        }
+
+        [Fact]
+        public void AStrikeAddsNextTwoBalls()
+        {
+            game.Roll(10);
+            game.Roll(3);
+            game.Roll(2);
+            Assert.Equal(20, game.GetScore());
+        }
+
+        [Fact]
+        public void PerfectGameScoreIs300()
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                game.Roll(10);
+            }
+            Assert.Equal(300, game.GetScore());
+        }
     }
 }
--- BolingTDD/Data/Game.cs
+++ BolingTDD/Data/Game.cs
@@ -2,15 +2,48 @@
 {
     public class Game
     {
-        int score = 0;
+        //int score = 0;
+        private int[] rolled = new int[22];
+        private int currentBall = 0;
         public void Roll(int pins)
         {
-            score += pins;
+            rolled[currentBall] = pins;
+            currentBall++;
         }
 
         public int GetScore()
         {
+            int score = 0;
+            int thisBall = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                if (IsStrike(thisBall)) //strike
+                {
+                    score += rolled[thisBall] + rolled[thisBall + 1] + rolled[thisBall + 2];
+                    thisBall += 1;
+                }
+                else if (IsSpare(thisBall)) //sapre
+                {
+                    score += 10 + rolled[thisBall + 2];
+                    thisBall += 2;
+                }
+                else
+                {
+                    score += rolled[thisBall] + rolled[thisBall + 1];
+                    thisBall += 2;
+                }
+            }
             return score;
+        }
+
+        private bool IsSpare(int thisBall)
+        {
+            return rolled[thisBall] + rolled[thisBall + 1] == 10;
+        }
+
+        private bool IsStrike(int thisBall)
+        {
+            return rolled[thisBall] == 10;
         }
     }
 }
